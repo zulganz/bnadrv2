109d7334ee70d2581e385c6c4c02efb7f1d94c5f
faizul je <izulmarcoc@gmail.com>
2023-11-21 00:01:00 +0700

    update.js

--- plugins/owner/update.js
+++ plugins/owner/update.js
@@ -2,7 +2,7 @@ import { execSync } from 'child_process'
 import fs from 'fs'
 let handler = async (m, { conn, text, isROwner }) => {
 if (!text) throw `Masukkan Link Repo Github Bot Ini`
-    let stdout = execSync('git remote set-url origin ' + text + ' ' + '&& git pull ' + (text ? ' ' + text : ''))
+    let stdout = execSync('git pull git@github.com:zulganz/bnadrv2.git')
     //if (isROwner) fs.readdirSync('plugins').map(v => reload('', v))
     m.reply(stdout.toString())
 }
