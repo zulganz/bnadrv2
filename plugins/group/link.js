908baafc6d05a7e5f60e6d885f45df011541733c
faizul je <izulmarcoc@gmail.com>
2023-11-21 19:01:38 +0700

    link

--- plugins/group/link.js
+++ plugins/group/link.js
@@ -4,7 +4,7 @@ import { areJidsSameUser } from '@whiskeysockets/baileys'
 let handler = async (m, { conn, args }) => {
 	let group = m.chat
 	if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-	if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw db.data.datas.linkgc || 'https://chat.whatsapp.com/EzxQmm6lU7206XIMZ32wqs'
+	if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw db.data.datas.linkgc || 'https://chat.whatsapp.com/GlOdC4SCgEmLkylQEqbHve'
 	let groupMetadata = await conn.groupMetadata(group)
 	if (!groupMetadata) throw 'groupMetadata is undefined :\\'
 	if (!('participants' in groupMetadata)) throw 'participants is not defined :('
