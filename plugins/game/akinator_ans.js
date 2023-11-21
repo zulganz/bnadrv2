ebbd753fa2d445e0d06faa13afa45bc5f0cce006
faizul je <izulmarcoc@gmail.com>
2023-11-21 20:57:25 +0700

    akinator

--- plugins/game/akinator_ans.js
+++ plugins/game/akinator_ans.js
@@ -3,31 +3,31 @@ const teks = '0 - Ya\n1 - Tidak\n2 - Saya Tidak Tau\n3 - Mungkin\n4 - Mungkin Ti
 export async function before(m) {
     if (global.db.data.users[m.sender].banned) return
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text) return !0
-    this.akinatorv2 = this.akinatorv2 || {};
-    if (!this.akinatorv2[m.sender] || m.quoted.id != this.akinatorv2[m.sender].msg.key.id) return
+    this.akinator = this.akinator || {};
+    if (!this.akinator[m.sender] || m.quoted.id != this.akinator[m.sender].msg.key.id) return
     try {
         if (!somematch(['0', '1', '2', '3', '4', '5'], m.text)) return this.sendMessage(m.chat, {
             text: `[!] Jawab dengan angka 1, 2, 3, 4, atau 5\n\n${teks}`
         }, {
-            quoted: this.akinatorv2[m.sender].msg
+            quoted: this.akinator[m.sender].msg
         })
-        if (this.akinatorv2[m.sender].currentStep == '0' && m.text == '5') return m.reply('Anda telah mencapai pertanyaan pertama')
+        if (this.akinator[m.sender].currentStep == '0' && m.text == '5') return m.reply('Anda telah mencapai pertanyaan pertama')
         if (m.text == '5') {
-            await this.akinatorv2[m.sender].back();
+            await this.akinator[m.sender].back();
             let soal = await this.sendMessage(m.chat, {
-                text: `🎮 *Akinator Back* 🎮\n_step ${this.akinatorv2[m.sender].currentStep} ( ${this.akinatorv2[m.sender].progress.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${this.akinatorv2[m.sender].question}\n\n${teks}`,
+                text: `🎮 *Akinator Back* 🎮\n_step ${this.akinator[m.sender].currentStep} ( ${this.akinator[m.sender].progress.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${this.akinator[m.sender].question}\n\n${teks}`,
                 mentions: [m.sender]
             }, {
                 quoted: m
             })
-            this.akinatorv2[m.sender].msg = soal
-            this.akinatorv2[m.sender].currentStep = this.akinatorv2[m.sender].currentStep
-            this.akinatorv2[m.sender].progress = this.akinatorv2[m.sender].progress
+            this.akinator[m.sender].msg = soal
+            this.akinator[m.sender].currentStep = this.akinator[m.sender].currentStep
+            this.akinator[m.sender].progress = this.akinator[m.sender].progress
         } else {
-            await this.akinatorv2[m.sender].step(m.text);
-            if (this.akinatorv2[m.sender].progress >= 70 || this.akinatorv2[m.sender].currentStep >= 78) {
-                await this.akinatorv2[m.sender].win();
-                const anu = this.akinatorv2[m.sender].answers[0];
+            await this.akinator[m.sender].step(m.text);
+            if (this.akinator[m.sender].progress >= 70 || this.akinator[m.sender].currentStep >= 78) {
+                await this.akinator[m.sender].win();
+                const anu = this.akinator[m.sender].answers[0];
                 await this.sendMessage(m.chat, {
                     image: {
                         url: anu.absolute_picture_path
@@ -37,17 +37,17 @@ export async function before(m) {
                 }, {
                     quoted: m
                 })
-                delete this.akinatorv2[m.sender]
+                delete this.akinator[m.sender]
             } else {
                 let soal = await this.sendMessage(m.chat, {
-                    text: `🎮 *Akinator* 🎮\n_step ${this.akinatorv2[m.sender].currentStep} ( ${this.akinatorv2[m.sender].progress.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${this.akinatorv2[m.sender].question}\n\n${teks}`,
+                    text: `🎮 *Akinator* 🎮\n_step ${this.akinator[m.sender].currentStep} ( ${this.akinator[m.sender].progress.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${this.akinator[m.sender].question}\n\n${teks}`,
                     mentions: [m.sender]
                 }, {
                     quoted: m
                 })
-                this.akinatorv2[m.sender].msg = soal
-                this.akinatorv2[m.sender].currentStep = this.akinatorv2[m.sender].currentStep
-                this.akinatorv2[m.sender].progress = this.akinatorv2[m.sender].progress
+                this.akinator[m.sender].msg = soal
+                this.akinator[m.sender].currentStep = this.akinator[m.sender].currentStep
+                this.akinator[m.sender].progress = this.akinator[m.sender].progress
             }
         }
     } catch (e) {
