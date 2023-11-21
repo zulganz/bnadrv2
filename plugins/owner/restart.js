13c68dc90885f2d52e2a906d471de0643fc53d04
faizul je <izulmarcoc@gmail.com>
2023-11-21 12:50:10 +0700

    maybe?

--- plugins/owner/restart.js
+++ plugins/owner/restart.js
@@ -1,5 +1,7 @@
 import { spawn } from 'child_process'
+import { fork } from 'cluster'
 let handler = async (m, { conn, isROwner, text }) => {
+    let p = fork()
     if (!p.send) throw 'Dont: node main.js\nDo: node index.js'
     if (!isROwner) throw '_eeeeeiiittsssss..._'
     await m.reply('```R E S T A R T . . .```')
