69bc0ecb1f208e7c37f659ae117714b891c4a87e
faizul je <izulmarcoc@gmail.com>
2023-11-21 10:30:16 +0700

    restart

---
+++ plugins/owner/restart.js
@@ -0,0 +1,15 @@
+import { spawn } from 'child_process'
+let handler = async (m, { conn, isROwner, text }) => {
+    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+    if (!isROwner) throw '_eeeeeiiittsssss..._'
+    await m.reply('```R E S T A R T . . .```')
+    process.send('reset')
+}
+
+handler.help = ['restart']
+handler.tags = ['owner']
+handler.command = /^(res(tart)?)$/i
+
+handler.rowner = true
+
+export default handler
