674f9af76b685c6474f6ada53e62acaa22504e1c
zulganz <izulmarcoc@gmail.com>
2023-11-22 14:56:30 +0700

    updt

--- plugins/group/creator.js
+++ plugins/group/creator.js
@@ -1,14 +1,9 @@
 import db from '../../lib/database.js'
 
 let handler = async (m, { conn, command, usedPrefix}) => {
-	try {
-	//const data = [...global.rowner.filter(([id, isCreator]) => id && isCreator)]
-	//if (data.length == 0) throw `*[ ! ] Real Owner belum di set.*\n\n*${usedPrefix}addrealowner* untuk menambahkan Owner Asli.\n\n*${usedPrefix}addowner* untuk menambahkan Owner Biasa.`
-	await conn.sendContact(m.chat, global.rowner.map(([id, name]) => [id, name]), m)
-	await m.reply('ini Nomor nya kak')
-	} catch (e) {
-		throw e
-	}
+	const data = [...db.data.datas.rowner.filter(([id, isCreator]) => id && isCreator), ...db.data.datas.owner.filter(([id, isCreator]) => id && isCreator)]
+	if (data.length == 0) throw `*[ ! ] Real Owner belum di set.*\n\n*${usedPrefix}addrealowner* untuk menambahkan Owner Asli.\n\n*${usedPrefix}addowner* untuk menambahkan Owner Biasa.`
+	await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
 
 handler.menugroup = ['owner']
