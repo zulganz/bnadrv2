814c3ddc9991f21535a6a50258a6e3effbac26c1
zulganz <izulmarcoc@gmail.com>
2023-11-22 15:36:31 +0700

    update

--- plugins/creator/attp.js
+++ plugins/creator/attp.js
@@ -1,22 +1,59 @@
-import { createSticker } from 'wa-sticker-formatter'
-import fs from 'fs'
-
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-	text = text ? text : m.quoted?.text ? m.quoted.text : m.quoted?.caption ? m.quoted.caption : m.quoted?.description ? m.quoted.description : ''
-	if (!text) throw `Example : ${usedPrefix + command} Lagi Ruwet`
-	try {
-		let stiker = await createSticker(`https://api.lolhuman.xyz/api/${command}?apikey=${api.lol}&text=${encodeURIComponent(text.substring(0, 151))}`, { pack: packname, author: author })
-		await conn.sendFile(m.chat, stiker, '', '', m)
-	} catch (e) {
-		console.log(e)
-		await conn.sendFile(m.chat, fs.readFileSync(`./media/sticker/bronya.webp`), '', '', m)
-	}
+var request = require('request');
+var {
+	sticker
+} = require('../lib/sticker.cjs');
+var handler = async (m, {
+	conn,
+	text
+}) => {
+	var teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+	var stiker = await sticker((await attp(teks)).result, global.packname, global.author)
+	conn.sendFile(m.chat, stiker, 'attp.webp', '', null, false, {
+		asSticker: true
+	})
 }
+handler.help = ['attp *teks*']
+handler.tags = ['sticker']
 
-handler.help = ['ttp','ttp2','ttp3','ttp4','ttp5','ttp6','attp','attp2']
-handler.tags = ['creator']
-handler.command = /^((ttp(2|3|4|5|6)?)|(attp2?))$/i
-
-handler.limit = true
+handler.command = /^attp$/i
 
 export default handler
+
+var attp = async (text) => {
+	return new Promise(async (resolve, reject) => {
+		const getid = await axios.get('https://id.bloggif.com/text')
+		const id = cheerio.load(getid.data)('form').attr('action')
+		const options = {
+			method: "POST",
+			url: `https://id.bloggif.com${id}`,
+			headers: {
+				"content-type": 'application/x-www-form-urlencoded',
+				"user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+			},
+			formData: {
+				target: 1,
+				text: text,
+				glitter_id: Math.floor(Math.random() * 2821),
+				font_id: 'lucida_sans_demibold_roman',
+				size: 50,
+				bg_color: 'FFFFFF',
+				transparent: 1,
+				border_color: '000000',
+				border_width: 2,
+				shade_color: '000000',
+				shade_width: 1,
+				angle: 0,
+				text_align: 'center'
+			},
+		};
+		request(options, async function(error, response, body) {
+			if (error) return new Error(error)
+			const $ = cheerio.load(body)
+			const url = $('.box.center > a').attr('href')
+			resolve({
+				status: 200,
+				result: 'https://id.bloggif.com' + url
+			})
+		})
+	})
+}
