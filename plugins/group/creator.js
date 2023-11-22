99583dbdd521ac7e9ab00dc3f5c6904bd7c9b16e
zulganz <izulmarcoc@gmail.com>
2023-11-22 15:05:56 +0700

    update

--- plugins/group/creator.js
+++ plugins/group/creator.js
@@ -1,13 +1,116 @@
-import db from '../../lib/database.js'
+import fetch from "node-fetch"
+import PhoneNumber from 'awesome-phonenumber'
+let handler = async (m, {
+    conn,
+    usedPrefix,
+    text,
+    args,
+    command
+}) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+    let name = await conn.getName(who)
 
-let handler = async (m, { conn, command, usedPrefix}) => {
-	const data = [...global.rowner.filter(([id, isCreator]) => id && isCreator)]
-	//if (data.length == 0) throw `*[ ! ] Real Owner belum di set.*\n\n*${usedPrefix}addrealowner* untuk menambahkan Owner Asli.\n\n*${usedPrefix}addowner* untuk menambahkan Owner Biasa.`
-	await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+    if (command == "creator") {
+        let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Bandar;Bot;;Md\nFN:Saya Owner Bandar Bot WhatsApp, Md\nNICKNAME:👑 Owner Bandar Bot\nORG:Zul\nTITLE:BnDR\nitem1.TEL;waid=6282281237299:+62 822-8123-7299\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://s.id/Cerdasin62\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:izul@mail.com\nitem3.X-ABLabel:💌 Mail Owner Bandar\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 14 april 2006\nEND:VCARD`
+        let tag_own = await conn.sendMessage(m.chat, {
+            contacts: {
+                displayName: "zul",
+                contacts: [{
+                    vcard
+                }]
+            },
+            contextInfo: {
+                externalAdReply: {
+                    title: "zul",
+                    mediaType: 1,
+                    previewType: 0,
+                    renderLargerThumbnail: true,
+                    sourceUrl: ''
+                }
+            }
+        }, {
+            quoted: fakes
+        })
+        await conn.reply(m.chat, `Halo kak @${m.sender.split("@")[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, tag_own, {
+            mentions: [m.sender]
+        })
+    }
+    if (command == "owner") {
+        try {
+            const ownerPromises = global.rowner.map(async (item, index) => [
+                item[0],
+                (await conn.getName(item[0] + "@s.whatsapp.net")) || "Tidak diketahui",
+                "👑 Owner",
+                ((await conn.fetchStatus(item[0] + "@s.whatsapp.net")).status) || "Tidak diketahui",
+                "zul@gmail.com",
+                "🇮🇩 Indonesia",
+                "🚀 https://github.com/zulganz",
+                "👤 Gada pawang nih senggol dong 😔"
+            ]);
+
+            const data = await Promise.all(ownerPromises);
+
+            let contacts = []
+            for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+                number = number.replace(/[^0-9]/g, '')
+                let njid = number + '@s.whatsapp.net'
+                let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+                // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
+                let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${isi}
+item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+item1.X-ABLabel:${isi1}
+item2.EMAIL;type=INTERNET:${isi2}
+item2.X-ABLabel:📧 Email
+item3.ADR:;;${isi3};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:📍 Region
+item4.URL:${isi4}
+item4.X-ABLabel:Website
+item5.X-ABLabel:${isi5}
+END:VCARD`.trim()
+                contacts.push({
+                    vcard,
+                    displayName: name
+                })
+            }
+            let tag_own = await conn.sendMessage(m.chat, {
+                contacts: {
+                    displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
+                    contacts,
+                },
+                contextInfo: {
+                    externalAdReply: {
+                        title: wm,
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: logo,
+                        sourceUrl: ''
+                    }
+                }
+            }, {
+                quoted: fakes
+            })
+            await conn.reply(m.chat, `Halo kak @${m.sender.split("@")[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, tag_own, {
+                mentions: [m.sender]
+            });
+        } catch {
+            let tag_own = await conn.sendContact(m.chat, nomorown, await conn.getName(nomorown + "@s.whatsapp.net"), m);
+            await conn.reply(m.chat, `Halo kak @${m.sender.split("@")[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, tag_own, {
+                mentions: [m.sender]
+            });
+        }
+
+    }
 }
-
-handler.menugroup = ['owner']
-handler.tagsgroup = ['group']
-handler.command = /^(owner|creator)$/i
+handler.help = ["owner", "creator", "pengembang"]
+handler.tags = ["info"]
+handler.command = /^(owner|pengembang|creator)$/i
 
 export default handler
