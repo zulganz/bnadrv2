1e504215cbe2a4b6a835e7808d57b9b0b8cce880
zulganz <izulmarcoc@gmail.com>
2023-11-23 16:07:47 +0700

    maybe??

--- plugins/tools/obfuscate.js
+++ plugins/tools/obfuscate.js
@@ -1,69 +1,56 @@
-import JavaScriptObfuscator from "javascript-obfuscator"
+import fs from 'fs';
+import JavaScriptObfuscator from 'javascript-obfuscator';
+import { conn } from '../../lib/connection';
 
-let handler = async (m, {
-    args,
-    command,
-    usedPrefix
-}) => {
-const usage = `*Example:*
-${usedPrefix}${command} (Input text or reply text to enc code)
-${usedPrefix}${command} doc (Reply to a document)`;
+const handler = async (m, { args, command }) => {
+    try {
+        const q = m.quoted || m;
+        const mime = (q.msg || q).mimetype || '';
 
-let text;
+        if (mime !== "application/js") {
+            return m.reply('Cuman Boleh File .js');
+        }
 
-if (args.length >= 1) {
-  text = args.join(" ");
-} else if (m.quoted && m.quoted.text) {
-  text = m.quoted.text;
-} else {
-  return m.reply(usage);
-}
+        // Download the media file
+        const buffer = await q.download();
 
-try {
-  if (text === 'doc' && m.quoted && m.quoted.mtype === 'documentMessage') {
-    let docBuffer;
-    if (m.quoted.mimetype) {
-      docBuffer = await m.quoted.download();
+        // Check the file size here
+        const fileSizeInBytes = buffer.length;
+        const fileSizeInMB = fileSizeInBytes / (1024 * 1024);
+
+        if (fileSizeInMB > 3) {
+            return m.reply('Input file size is too large. It must be below 3 MB.');
+        }
+
+        const readjs = fs.readFileSync(buffer);
+
+        const result = await Encrypt(readjs);
+        conn.sendFile(m.chat, result, 'obfus.js', 'Nih', m);
+
+    } catch (err) {
+        console.error(`Terjadi kesalahan: ${err.message}`);
+        return m.reply(`Terjadi kesalahan saat mengobfuskasi file: ${err.message}`);
     }
-    const message = await Encrypt(docBuffer.toString('utf-8'));
-    await m.reply(message);
-  }
-  //if the file is js
-  else if (text.endsWith('js')) {
-    let docBuffer;
-    if (m.quoted.mimetype) {
-      docBuffer = await m.quoted.download();
-    }
-    let input = fs.readFileSync(docBuffer, 'utf8');
-    let message = await Encrypt(input);
-    await m.reply(message);
-  }
-  else {
-    const message = await Encrypt(text);
-    await m.reply(message);
-  }
-} catch (error) {
-  const errorMessage = `Terjadi kesalahan: ${error.message}`;
-  await m.reply(errorMessage);
-}
+};
 
-}
-handler.command = /^(obfus(cate)?|enc)$/i
-export default handler
+handler.command = /^(obfus)$/i;
+
+export default handler;
+
 
 async function Encrypt(query) {
-    const obfuscationResult = JavaScriptObfuscator.obfuscate(query, {
-        compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 1,
-        numbersToExpressions: true,
-        simplify: true,
-        stringArrayShuffle: true,
-        splitStrings: true,
-        stringArrayThreshold: 1,
-        sourceMap: false,
-        sourceMapMode: "separate",
-    })
+  const obfuscationResult = JavaScriptObfuscator.obfuscate(query, {
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 1,
+      numbersToExpressions: true,
+      simplify: true,
+      stringArrayShuffle: true,
+      splitStrings: true,
+      stringArrayThreshold: 1,
+      sourceMap: false,
+      sourceMapMode: "separate",
+  })
 
-    return obfuscationResult.getObfuscatedCode()
+  return obfuscationResult.getObfuscatedCode()
 }
