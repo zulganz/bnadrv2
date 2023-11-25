5130a16d3396ff762fc826e67f3efbaac5a34a08
zulganz <izulmarcoc@gmail.com>
2023-11-25 16:40:38 +0700

    gdsg

--- plugins/_functionbefore/_autobackup.js
+++ plugins/_functionbefore/_autobackup.js
@@ -5,7 +5,7 @@ let cooldowns = '1000 * 60 * 60 * 3'
 export async function all(m) {
     //let time = moment.tz('Asia/Jakarta').format('HH')
     let cooldown = db.data.datas.backup
-    if (new Date() > cooldown) return !0
+    if (new Date() < cooldown) return !0
         await this.reply(m.chat, `Backup file`, m)
         await this.sendMessage(global.rowner[0] + '@s.whatsapp.net', { document: fs.readFileSync('./database.json'), fileName: 'database.json', mimetype: 'application/json' }, { quoted: null })
 		await this.sendMessage(global.rowner[0] + '@s.whatsapp.net', { document: await fs.readFileSync('./sessions/creds.json'), fileName: 'creds.json', mimetype: 'application/json' }, { quoted: null })
