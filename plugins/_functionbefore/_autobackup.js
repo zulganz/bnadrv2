0d4000e81fb2d0a94216d08fed0fb52be9064be9
zulganz <izulmarcoc@gmail.com>
2023-11-25 16:23:32 +0700

    asda

--- plugins/_functionbefore/_autobackup.js
+++ plugins/_functionbefore/_autobackup.js
@@ -2,7 +2,6 @@ import moment from 'moment-timezone'
 let cooldown = '1000 * 60 * 60 * 3'
 export async function all() {
     //let time = moment.tz('Asia/Jakarta').format('HH')
-    if (!isMods) return !0
     if (+new Date() > cooldown) {
         await this.reply(m.chat, `Backup file`, m)
         await conn.sendMessage(global.rowner[0] + '@s.whatsapp.net', { document: fs.readFileSync('./database.json'), fileName: 'database.json', mimetype: 'application/json' }, { quoted: null })
