08ec8ed944ebcd2f80c070de214a3a6dcb442992
zulganz <izulmarcoc@gmail.com>
2023-11-25 16:44:27 +0700

    r

--- plugins/_functionbefore/_autobackup.js
+++ plugins/_functionbefore/_autobackup.js
@@ -1,4 +1,4 @@
-import moment from 'moment-timezone'
+/*import moment from 'moment-timezone'
 import fs from 'fs'
 import db from '../../lib/database.js'
 let cooldowns = '1000 * 60 * 60 * 3'
@@ -10,4 +10,4 @@ export async function all(m) {
         await this.sendMessage(global.rowner[0] + '@s.whatsapp.net', { document: fs.readFileSync('./database.json'), fileName: 'database.json', mimetype: 'application/json' }, { quoted: null })
 		await this.sendMessage(global.rowner[0] + '@s.whatsapp.net', { document: await fs.readFileSync('./sessions/creds.json'), fileName: 'creds.json', mimetype: 'application/json' }, { quoted: null })
         cooldown += new Date() * cooldowns
-}
+}*/
