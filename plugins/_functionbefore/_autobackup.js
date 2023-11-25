c61684e0500348dd323e50e68bbeaaa9fe4e37ed
zulganz <izulmarcoc@gmail.com>
2023-11-25 16:40:17 +0700

    asda

--- plugins/_functionbefore/_autobackup.js
+++ plugins/_functionbefore/_autobackup.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fs from 'fs'
 import db from '../../lib/database.js'
 let cooldowns = '1000 * 60 * 60 * 3'
-export async function before(m) {
+export async function all(m) {
     //let time = moment.tz('Asia/Jakarta').format('HH')
     let cooldown = db.data.datas.backup
     if (new Date() > cooldown) return !0
