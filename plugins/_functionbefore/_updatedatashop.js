4d74c6deba366e2b7125570e065c6fcd1e1cb843
zulganz <izulmarcoc@gmail.com>
2023-11-28 23:12:29 +0700

    maybee??

--- plugins/_functionbefore/_updatedatashop.js
+++ plugins/_functionbefore/_updatedatashop.js
@@ -1,12 +1,17 @@
 import db from '../../lib/database.js';
 import { ranNumb } from '../../lib/func.js';
 
-export async function before (m, {conn}) {
-    try {
+export async function all(m, {conn}) {
     let datas = db.data.datas;
     let cooldown = 1000 * 60 * 1; // 24 hours in millisecond
 
     if (new Date() * 1 - datas.lastupdategraph > cooldown) {
+        let d = new Date()
+        let date = d.toLocaleDateString('id', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+        })
         this.reply(global.rowner[0] + '@s.whatsapp.net', `Update shop: ${date}`, null)
         let rannumba = ranNumb(10, 10000);
         let rannumbb = ranNumb(10, 10000);
@@ -17,7 +22,5 @@ export async function before (m, {conn}) {
         let caption = `*Graph Shop*\n\nBanana: ${formatRupiah(rannumba)}\nApple: ${formatRupiah(rannumbb)}\nWatermelon: ${formatRupiah(rannumbc)}`;
         this.reply(global.rowner[0] + '@s.whatsapp.net', caption, null)
     }
-} catch (e) {
-    console.log(e)
-}
+    return !0
 }
