45406c686ccbdddeb2edd42d962d90a249f23a12
zulganz <izulmarcoc@gmail.com>
2023-11-29 21:55:54 +0700

    yead

--- plugins/download/ig.js
+++ plugins/download/ig.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     try {
     if (!args[0]) throw `*Perintah ini untuk mengunduh video dari instagram dengan link*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CFqX7ZpJfV0/?utm_medium=copy_link`
     if (!args[0].match(/instagram\.com\/(?:reel|p|tv|([A-Za-z0-9-_.]+\/(reel|p|tv)))\/[A-Za-z0-9-_.]+/g)) throw `*Link salah! Perintah ini untuk mengunduh video dari instagram dengan link*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CFqX7ZpJfV0/?utm_medium=copy_link`
-    instagram.download(args[0]).then(async res => {
+    Instagram.download(args[0]).then(async res => {
         conn.sendFile(m.chat, res, 'ig' + args[0].substr(args[0].lastIndexOf('.')), null, m)
     })
 } catch (e) {
