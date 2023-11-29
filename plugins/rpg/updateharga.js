97cfc7517133ea1a9937b0bbbb4d3923ef9aec70
zulganz <izulmarcoc@gmail.com>
2023-11-29 10:53:05 +0700

    y

---
+++ plugins/rpg/updateharga.js
@@ -0,0 +1,12 @@
+import db from '../../lib/database.js'
+
+let handler = async (m, { conn }) => {
+    let datas = db.data.datas  
+    let caption = `*Graph Shop*\n\nBanana: ${formatRupiah(datas.graphshopbanana)}\nApple: ${formatRupiah(datas.graphshopapple)}\nWatermelon: ${formatRupiah(datas.graphshopwatermelon)}`
+    conn.sendMessage(m.chat, caption, m)
+}
+handler.tags = ['rpg']
+handler.tagsfun = ['rpg']
+handler.command = /^(updateharga)$/i
+handler.owner = true
+export default handler
